14085efa7b6bbda1b324179033427e1f9b3a0c05
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-16 13:55:06 +0700

    submit file manipulation-dictionary.py

--- manipulation-dictionary.py
+++ manipulation-dictionary.py
@@ -3,30 +3,34 @@ print(">>> Fitur .clear()")
 info_karyawan = {'nama' : 'Aksara',
                  'nik' : '1211011',
                  'pekerjaan' : 'Data Analyst'}
-___.___
+info_karyawan.clear()
 print(info_karyawan)
+
 # Fitur .copy()
 print(">>> Fitur .copy()")
 info_karyawan1 = {'nama' : 'Aksara',
                   'nik' : '1211011',
                   'pekerjaan' : 'Data Analyst'}
-info_karyawan2 = ___.___
+info_karyawan2 = info_karyawan1.copy()
 info_karyawan2['nama'] = 'Senja'
 info_karyawan2['nik'] = '1211056'
 print(info_karyawan1)
 print(info_karyawan2)
+
 # Fitur .keys()
 print(">>> Fitur .keys()")
 info_karyawan = {'nama' : 'Aksara',
                  'nik' : '1211011',
                  'pekerjaan' : 'Data Analyst'}
-kunci_akses = list(___.___)
+kunci_akses = list(info_karyawan.keys())
 print(kunci_akses)
+
 # Fitur .values()
 print(">>> Fitur .values()")
-value_dict = list(___.___)
+value_dict = list(info_karyawan.values())
 print(value_dict)
+
 # Fitur .update()
 print(">>> Fitur .update()")
-___.___({'skillset':['Python', 'R']})
+info_karyawan.update({'skillset':['Python', 'R']})
 print(info_karyawan)
