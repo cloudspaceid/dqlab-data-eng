8b756f3321b9a51481f144f5dc28f39da57ef15c
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-16 13:10:42 +0700

    add file manipulation-dictionary.py

---
+++ manipulation-dictionary.py
@@ -0,0 +1,32 @@
+# Fitur .clear()
+print(">>> Fitur .clear()")
+info_karyawan = {'nama' : 'Aksara',
+                 'nik' : '1211011',
+                 'pekerjaan' : 'Data Analyst'}
+___.___
+print(info_karyawan)
+# Fitur .copy()
+print(">>> Fitur .copy()")
+info_karyawan1 = {'nama' : 'Aksara',
+                  'nik' : '1211011',
+                  'pekerjaan' : 'Data Analyst'}
+info_karyawan2 = ___.___
+info_karyawan2['nama'] = 'Senja'
+info_karyawan2['nik'] = '1211056'
+print(info_karyawan1)
+print(info_karyawan2)
+# Fitur .keys()
+print(">>> Fitur .keys()")
+info_karyawan = {'nama' : 'Aksara',
+                 'nik' : '1211011',
+                 'pekerjaan' : 'Data Analyst'}
+kunci_akses = list(___.___)
+print(kunci_akses)
+# Fitur .values()
+print(">>> Fitur .values()")
+value_dict = list(___.___)
+print(value_dict)
+# Fitur .update()
+print(">>> Fitur .update()")
+___.___({'skillset':['Python', 'R']})
+print(info_karyawan)
