2822be2cb07b0290995d16ba6bb3c184c4583f6b
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-16 12:58:28 +0700

    add file manipulation-set2.py

---
+++ manipulation-set2.py
@@ -0,0 +1,47 @@
+# Fitur .union()
+print(">>> Fitur .union()")
+parcel1 = {'Anggur','Apel','Jeruk'}
+parcel2 = {'Apel','Kiwi','Melon'}
+parcel3 = ___.___(___)
+print(parcel1)
+print(parcel3)
+# Fitur .isdisjoint()
+print(">>> Fitur .isdisjoint()")
+parcel1 = {'Anggur','Apel','Jeruk'}
+parcel2 = {'Kiwi','Melon','Pisang'}
+parcel3 = {'Apel','Srikaya','Semangka'}
+parcel1_parcel2_disjoint = ___.___(___)
+print(parcel1_parcel2_disjoint)
+parcel1_parcel3_disjoint = ___.___(___)
+print(parcel1_parcel3_disjoint)
+# Fitur .issubset()
+print(">>> Fitur .issubset()")
+parcel_A = {'Anggur', 'Apel'}
+parcel_B = {'Durian','Semangka','Apel'}
+parcel_C = {'Anggur', 'Kiwi', 'Apel', 'Jeruk', 'Melon'}
+parcel_A_dalam_C = ___.___(___)
+parcel_B_dalam_C = ___.___(___)
+print(parcel_A_dalam_C)
+print(parcel_B_dalam_C)
+# Fitur .issuperset()
+print(">>> Fitur .issuperset()")
+parcel_C_mengandung_A = ___.___(___)
+parcel_C_mengandung_B = ___.___(___)
+print(parcel_C_mengandung_A)
+print(parcel_C_mengandung_B)
+# Fitur .intersection()
+print(">>> Fitur .intersection()")
+parcel_A = {'Anggur', 'Kiwi', 'Apel', 'Jeruk', 'Melon'}
+parcel_B = {'Apel', 'Jeruk', 'Semangka', 'Durian', 'Tomat'}
+parcel_C = ___.___(___)
+print(parcel_C)
+# Fitur .difference()
+print(">>> Fitur .difference()")
+parcel_C = ___.___(___)
+print(parcel_C)
+# Fitur .symmetric_difference()
+print(">>> Fitur .symmetric_difference()")
+parcel_A = {'Anggur', 'Apel', 'Jeruk', 'Melon'}
+parcel_B = {'Apel','Jeruk','Semangka','Leci'}
+parcel_C = ___.___(___)
+print(parcel_C)
