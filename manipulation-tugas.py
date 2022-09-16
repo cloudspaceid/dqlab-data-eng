f9a9187457d30c6be2c45a804dfb1a4e051042cc
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-16 14:02:30 +0700

    add file manipulation-tugas.py

---
+++ manipulation-tugas.py
@@ -0,0 +1,16 @@
+# Data keuangan
+keuangan = {
+'pengeluaran': [2, 2.5, 2.25, 2.5, 3.2, 2.5, 3.5, 4, 3],
+'pemasukan': [7.8, 7.5, 9, 7.6, 7.2, 7.5, 7, 10, 7.5]
+}
+# Perhitungan rata-rata pemasukan dan rata-rata pengeluaran
+total_pengeluaran = ___
+total_pemasukan = ___
+for biaya in ___['pengeluaran']:
+    ___
+for biaya in ___:
+    ___
+rata_rata_pengeluaran = ___
+rata_rata_pemasukan = ___
+print(rata_rata_pengeluaran)
+print(rata_rata_pemasukan)
