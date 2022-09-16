cd7fdfc774a8dea8d86237935f41cc26cc397b7a
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-16 11:25:31 +0700

    add file manipulation-set1.py

---
+++ manipulation-set1.py
@@ -0,0 +1,36 @@
+# Fitur .add()
+print(">>> Fitur .add()")
+set_buah = {'Jeruk','Apel','Anggur'}
+___.___(___)
+print(set_buah)
+# Fitur .clear()
+print(">>> Fitur .clear()")
+set_buah = {'Jeruk','Apel','Anggur'}
+___.___
+print(set_buah)
+# Fitur .copy()
+print(">>> Fitur .copy()")
+set_buah1 = {'Jeruk','Apel','Anggur'}
+set_buah2 = ___
+set_buah3 = ___.___
+set_buah2.add('Melon')
+set_buah3.add('Kiwi')
+print(set_buah1)
+print(set_buah2)
+# Fitur .update()
+print(">>> Fitur .update()")
+parcel1 = {'Anggur','Apel','Jeruk'}
+parcel2 = {'Apel','Kiwi','Melon'}
+___.___(___)
+print(parcel1)
+# Fitur .pop()
+print(">>> Fitur .pop()")
+parcel = {'Anggur','Apel','Jeruk'}
+buah = ___.___
+print(buah)
+print(parcel)
+# Fitur .remove()
+print(">>> Fitur .remove()")
+parcel = {'Anggur','Apel','Jeruk'}
+___.___(___)
+print(parcel)
