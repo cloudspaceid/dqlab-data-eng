78e6fb18678f992b282daf55ac86b5aa6dac63a5
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-16 12:57:38 +0700

    submit file manipulation-set1.py

--- manipulation-set1.py
+++ manipulation-set1.py
@@ -1,36 +1,41 @@
 # Fitur .add()
 print(">>> Fitur .add()")
 set_buah = {'Jeruk','Apel','Anggur'}
-___.___(___)
+set_buah.add('Melon')
 print(set_buah)
+
 # Fitur .clear()
 print(">>> Fitur .clear()")
 set_buah = {'Jeruk','Apel','Anggur'}
-___.___
+set_buah.clear()
 print(set_buah)
+
 # Fitur .copy()
 print(">>> Fitur .copy()")
 set_buah1 = {'Jeruk','Apel','Anggur'}
-set_buah2 = ___
-set_buah3 = ___.___
+set_buah2 = set_buah1
+set_buah3 = set_buah1.copy()
 set_buah2.add('Melon')
 set_buah3.add('Kiwi')
 print(set_buah1)
 print(set_buah2)
+
 # Fitur .update()
 print(">>> Fitur .update()")
 parcel1 = {'Anggur','Apel','Jeruk'}
 parcel2 = {'Apel','Kiwi','Melon'}
-___.___(___)
+parcel1.update(parcel2)
 print(parcel1)
+
 # Fitur .pop()
 print(">>> Fitur .pop()")
 parcel = {'Anggur','Apel','Jeruk'}
-buah = ___.___
+buah = parcel.pop()
 print(buah)
 print(parcel)
+
 # Fitur .remove()
 print(">>> Fitur .remove()")
 parcel = {'Anggur','Apel','Jeruk'}
-___.___(___)
+parcel.remove('Apel')
 print(parcel)
