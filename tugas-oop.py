bb459ec1c0dd9e94903ee82731c457a8b6231379
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-23 10:04:39 +0700

    add file de-part3-oop-encapsulation.py

--- tugas-oop.py
+++ tugas-oop.py
@@ -1,24 +1,24 @@
 # Definisikan class Karyawan
 class Karyawan:
-    def ___(self, nama, usia, pendapatan, insentif_lembur):
-        self.nama = ___
-        self.usia = ___
-        self.pendapatan = ___
+    def __init__(self, nama, usia, pendapatan, insentif_lembur):
+        self.nama = nama
+        self.usia = usia
+        self.pendapatan = pendapatan
         self.pendapatan_tambahan = 0
-        self.insentif_lembur = ___
+        self.insentif_lembur = insentif_lembur
     def lembur(self):
-        self.pendapatan_tambahan += ___
+        self.pendapatan_tambahan += self.insentif_lembur
     def tambahan_proyek(self, jumlah_tambahan):
-        self.pendapatan_tambahan += ___
+        self.pendapatan_tambahan += jumlah_tambahan
     def total_pendapatan(self):
-        return ___
+        return self.total_pendapatan
 # Definisikan class Perusahaan
 class Perusahaan:
-    def ___(self, nama, alamat, nomor_telepon):
-        self.nama = ___
-        self.alamat = ___
-        self.nomor_telepon = ___
-        self.list_karyawan = ___
+    def __init__(self, nama, alamat, nomor_telepon):
+        self.nama = nama
+        self.alamat = alamat
+        self.nomor_telepon = nomor_telepon
+        self.list_karyawan = 0
     def aktifkan_karyawan(self, karyawan):
         self.list_karyawan.___(___)
     def nonaktifkan_karyawan(self, nama_karyawan):
